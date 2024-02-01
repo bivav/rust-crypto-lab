7a8b2783f9d8fcff84b2983e3445ffbdc0c3431f
bivav <bivavraj@gmail.com>
2024-02-01 01:05:55 +0000

    Optimized code for error handling, Decrypt fn implemented, TODO: improve Key handling

--- file_encrypt_decrypt_aes/src/lib.rs
+++ file_encrypt_decrypt_aes/src/lib.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
+use std::fs;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::process::exit;
 
 use base64::Engine;
 use ring::aead;
+use ring::aead::{Aad, LessSafeKey, Nonce, UnboundKey};
 use ring::rand::{SecureRandom, SystemRandom};
 
 pub struct Config {
@@ -23,52 +25,87 @@ impl Config {
 
         Ok(Config { command, file_path })
     }
+
+    pub fn read_file(config: Config) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut file = File::open(config.file_path)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+        Ok(buffer)
+    }
+
+    pub fn read_file_base64(config: Config) -> Result<Vec<u8>, Box<dyn Error>> {
+        let file_content = fs::read_to_string(config.file_path)?;
+
+        let decoded_data = base64::engine::general_purpose::STANDARD.decode(&file_content)
+            .unwrap_or_else(|e| {
+                println!("Decoding Error: {:?}\nAre you sure it's the encrypted file?", e);
+                exit(1);
+            });
+
+        Ok(decoded_data)
+    }
+
+    pub fn save_as_base64_encoded_file(data: Vec<u8>, filename: &str) -> Result<(), Box<dyn Error>> {
+        let mut file = File::create(filename)?;
+        let encoded_data = base64::engine::general_purpose::STANDARD.encode(&data);
+        file.write_all(encoded_data.as_bytes())?;
+        Ok(())
+    }
+
+
+    pub fn save_file(data: String, filename: &str) -> Result<(), Box<dyn Error>> {
+        let mut file = File::create(filename)?;
+        file.write(&data.into_bytes())?;
+        Ok(())
+    }
 }
 
-pub fn run(config: Config) -> Result<Vec<u8>, Box<dyn Error>> {
-    // let file_content = fs::read_to_string(config.file_path)?;
-
-    let mut file = File::open(config.file_path)?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
-
-    Ok(buffer)
-}
-
-pub fn save_file(data: Vec<u8>, filename: &str) -> Result<(), Box<dyn Error>> {
-    let mut file = File::create(filename)?;
-    let encoded_data = base64::engine::general_purpose::STANDARD.encode(&data);
-    file.write_all(encoded_data.as_bytes())?;
-
-    Ok(())
-}
 
 pub struct FileEncryptDecrypt {}
 
 impl FileEncryptDecrypt {
-    pub fn encrypt(file_content: &mut Vec<u8>) -> ([u8; 12], &mut Vec<u8>) {
+    pub fn encrypt(file_content: &mut Vec<u8>) -> Result<([u8; 12], &mut Vec<u8>), Box<dyn Error>> {
         let rng = SystemRandom::new(); // Random Number Generator
         let mut encryption_key = [0u8; 32]; // Creating list of 256 bits of 0s (Encryption Key)
         rng.fill(&mut encryption_key).unwrap(); // Replacing the list using RNG
 
         let mut iv = [0u8; 12]; // Initialization Vector
         rng.fill(&mut iv).unwrap(); // Replacing the list using RNG
+        let nonce = Nonce::assume_unique_for_key(iv);
 
-        let unbound_key = aead::UnboundKey::new(&aead::AES_256_GCM, &encryption_key)
-            .unwrap_or_else(|err| {
-                println!("Error creating UnboundKey: {:?}", err);
-                exit(1);
-            });
+        // TODO: Important to remove this block of code.
+        let mut file = File::create("key.txt").expect("Error");
+        file.write_all(&encryption_key).expect("TODO: panic message");
 
-        let aead_key = aead::LessSafeKey::new(unbound_key);
+        let unbound_key = UnboundKey::new(&aead::AES_256_GCM, &encryption_key)
+            .map_err(|e| format!("Failed to create unbound key {}", e))?;
 
-        let nonce = aead::Nonce::assume_unique_for_key(iv);
+        let aead_key = LessSafeKey::new(unbound_key);
 
-        aead_key.seal_in_place_append_tag(nonce, aead::Aad::from(&[]), file_content)
-            .unwrap_or_else(|err| {
-                println!("Error Encrypting the text content: {:?}", err);
-            });
+        aead_key.seal_in_place_append_tag(nonce, Aad::from(&[]), file_content)
+            .map_err(|e| format!("Encryption failed: {:?}", e))?;
 
-        (iv, file_content)
+        Ok((iv, file_content))
+    }
+
+    pub fn decrypt(file_content: &mut Vec<u8>, _key: [u8; 3]) -> Result<String, Box<dyn Error>> {
+        // Read the encryption key from a file.
+        // TODO: Important to remove this block of code.
+        let key = fs::read("key.txt")?;
+
+        let iv = &file_content[0..12];
+        let nonce = Nonce::assume_unique_for_key(iv.try_into()?);
+
+        let unbound_key = UnboundKey::new(&aead::AES_256_GCM, &key)
+            .map_err(|e| format!("Failed to create unbound key: {:?}", e))?;
+        let aead_key = LessSafeKey::new(unbound_key);
+
+        let decrypted_data = aead_key.open_in_place(nonce, Aad::empty(), &mut file_content[12..])
+            .map_err(|e| format!("Decryption failed: {:?}", e))?;
+
+        let result = String::from_utf8(decrypted_data.to_vec())
+            .map_err(|e| format!("UTF-8 conversion failed: {:?}", e))?;
+
+        Ok(result)
     }
 }
--- file_encrypt_decrypt_aes/src/main.rs
+++ file_encrypt_decrypt_aes/src/main.rs
@@ -1,41 +1,29 @@
 use std::env;
-use std::process::exit;
+use std::error::Error;
 
-use file_encrypt_decrypt_aes::{Config, FileEncryptDecrypt, run, save_file};
+use file_encrypt_decrypt_aes::{Config, FileEncryptDecrypt};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        exit(1);
-    });
+    let config: Config = Config::build(&args)?;
 
     if config.command == "encrypt" {
-        let mut file_content_buffer = run(config).unwrap_or_else(|err| {
-            println!("Application error: {err}");
-            exit(1);
-        });
-
-        let (iv, cipher_text) = FileEncryptDecrypt::encrypt(&mut file_content_buffer);
+        let mut file_content_buffer = Config::read_file(config)?;
+        let (iv, cipher_text) = FileEncryptDecrypt::encrypt(&mut file_content_buffer)?;
 
         let mut encrypted_data = Vec::new();
         encrypted_data.extend_from_slice(&iv);
         encrypted_data.extend_from_slice(&cipher_text);
 
-        if let Err(e) = save_file(encrypted_data.clone(), "file.txt") {
-            println!("Saving error: {}", e);
-            exit(1);
-        }
-
-        println!("Encrypted: {}\n{:?}", cipher_text.len(), &cipher_text);
-        println!("Nonce (hex): {:?}\nCiphertext (hex): {:?}", hex::encode(iv), hex::encode(&encrypted_data));
+        Config::save_as_base64_encoded_file(encrypted_data, "file.txt")?;
     } else if config.command == "decrypt" {
-        println!("Decryption coming soon");
-        exit(0);
-    } else {
-        println!("Error:\nusage: cargo run -- [command] [file_path] ");
-        exit(1);
-    }
-}
+        let mut file_content_as_buffer = Config::read_file_base64(config)?;
+        let decrypted_text = FileEncryptDecrypt::decrypt(&mut file_content_as_buffer, [1, 2, 3])?;
 
+        Config::save_file(decrypted_text, "decrypted.txt")?;
+    } else {
+        return Err("Invalid command. Usage: cargo run -- [command] [file_path]".into());
+    }
+
+    Ok(())
+}
