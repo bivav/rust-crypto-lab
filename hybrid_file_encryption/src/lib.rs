064775d8a9513a4e52be001472d6512bd504cd46
bivav <bivavraj@gmail.com>
2024-02-10 23:49:55 +0000

    RSA encryption - Keypair generation implemented

--- hybrid_file_encryption/src/lib.rs
+++ hybrid_file_encryption/src/lib.rs
@@ -1,16 +1,18 @@
 use std::cmp::min;
-use std::error::Error;
+// use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::num::NonZeroU32;
 use std::process::exit;
 
-use anyhow::{bail, Result};
+use anyhow::{anyhow, bail, Result};
 use base64::Engine;
 use ring::aead::{Aad, LessSafeKey, Nonce, UnboundKey};
 use ring::rand::{SecureRandom, SystemRandom};
 use ring::{aead, digest, pbkdf2};
+use rsa::rand_core::OsRng;
+use rsa::{Pkcs1v15Encrypt, RsaPublicKey};
 
 pub struct Config {
     pub command: String,
@@ -42,10 +44,7 @@ impl Config {
         let decoded_data = base64::engine::general_purpose::STANDARD
             .decode(&file_content)
             .unwrap_or_else(|e| {
-                println!(
-                    "Decoding Error: {:?}\nAre you sure it's the encrypted file?",
-                    e
-                );
+                println!("Decoding Error: {:?}\nAre you sure it's the encrypted file?", e);
                 exit(1);
             });
 
@@ -83,10 +82,12 @@ impl FileEncryptDecrypt {
         decrypted_hash == encrypted_hash
     }
 
-    pub fn encrypt(
-        file_content: &mut Vec<u8>,
+    pub fn encrypt<'a>(
+        file_content: &'a mut Vec<u8>,
         password: String,
-    ) -> Result<([u8; 12], &mut Vec<u8>, [u8; 32]), Box<dyn Error>> {
+        public_key: &'a RsaPublicKey,
+        mut os_rng: OsRng,
+    ) -> Result<([u8; 12], &'a mut Vec<u8>, [u8; 32], Vec<u8>)> {
         let rng = SystemRandom::new(); // Random Number Generator
 
         // let mut encryption_key = [0u8; 32]; // Creating list of 256 bits of 0s (Encryption Key)
@@ -99,7 +100,6 @@ impl FileEncryptDecrypt {
         rng.fill(&mut iv).unwrap(); // Generating unique IV
 
         let nonce = Nonce::assume_unique_for_key(iv);
-
         let password_as_bytes = password.into_bytes();
 
         let mut encryption_key = [0u8; digest::SHA256_OUTPUT_LEN];
@@ -120,25 +120,25 @@ impl FileEncryptDecrypt {
             &encryption_key[..min(encryption_key.len(), 4)]
         );
 
+        let encrypt_symmetric_key = public_key.encrypt(&mut os_rng, Pkcs1v15Encrypt, &encryption_key)?;
+
+
         let unbound_key = UnboundKey::new(&aead::AES_256_GCM, &encryption_key)
-            .map_err(|e| format!("Failed to create unbound key {}", e))?;
+            .map_err(|e| anyhow!("Failed to create unbound key {}", e))?;
 
         let aead_key = LessSafeKey::new(unbound_key);
 
         aead_key
             .seal_in_place_append_tag(nonce, Aad::from(&[]), file_content)
-            .map_err(|e| format!("Encryption failed: {:?}", e))?;
+            .map_err(|e| anyhow!("Encryption failed: {:?}", e))?;
 
         println!("Final encrypted data length: {}", file_content.len());
 
-        Ok((iv, file_content, salt))
+        Ok((iv, file_content, salt, encrypt_symmetric_key))
     }
 
-    pub fn decrypt(file_content: &mut Vec<u8>, key: &[u8]) -> Result<String, Box<dyn Error>> {
-        println!(
-            "File content length before decrypting: {}",
-            file_content.len()
-        );
+    pub fn decrypt(file_content: &mut Vec<u8>, key: &[u8]) -> Result<String> {
+        println!("File content length before decrypting: {}", file_content.len());
 
         let salt = &file_content[32..64];
         let iv = &file_content[64..76];
@@ -166,7 +166,7 @@ impl FileEncryptDecrypt {
         let nonce = Nonce::assume_unique_for_key(iv.try_into()?);
 
         let unbound_key = UnboundKey::new(&aead::AES_256_GCM, &encryption_key)
-            .map_err(|e| format!("Failed to create unbound key: {:?}", e))?;
+            .map_err(|e| anyhow!("Failed to create unbound key: {:?}", e))?;
 
         let aead_key = LessSafeKey::new(unbound_key);
 
@@ -175,10 +175,10 @@ impl FileEncryptDecrypt {
 
         let decrypted_data = aead_key
             .open_in_place(nonce, Aad::empty(), &mut file_content[76..])
-            .map_err(|_| "Decryption failed: Issue with the key".to_string())?;
+            .map_err(|e| anyhow!("Decryption failed: Issue with the key {}", e))?;
 
         let result = String::from_utf8(decrypted_data.to_vec())
-            .map_err(|e| format!("UTF-8 conversion failed: {:?}", e))?;
+            .map_err(|e| anyhow!("UTF-8 conversion failed: {:?}", e))?;
 
         Ok(result)
     }
