be87fa9ad9591cc761423c9691ac6fc5cb81d2e6
bivav <bivavraj@gmail.com>
2024-02-20 18:32:23 +0000

    Created transactions for calculating merkel root calculations

--- simple-blockchain/src/lib.rs
+++ simple-blockchain/src/lib.rs
@@ -1,12 +1,19 @@
 use ring::digest;
 use ring::digest::SHA256;
 
+struct Transaction {
+    sender: String,
+    receiver: String,
+    amount: f32,
+}
+
 struct Block {
     index: u32,
     timestamp: u64,
-    data: String,
+    data: Vec<Transaction>,
     previous_hash: String,
     hash: String,
+    merkle_root: String,
     nonce: u64,
 }
 
@@ -24,12 +31,15 @@ impl Blockchain {
     }
 
     fn genesis_block() -> Block {
+        let transactions = vec![];
+
         Block {
             index: 0,
             timestamp: 0,
-            data: "Genesis Block".to_string(),
+            data: transactions.clone(),
             previous_hash: "0".to_string(),
             hash: "0".to_string(),
+            merkle_root: Self::calculate_merkel_root(&transactions),
             nonce: 0,
         }
     }
@@ -37,5 +47,15 @@ impl Blockchain {
     fn calculate_hash(data: &[u8]) -> String {
         let hasher = digest::digest(&SHA256, data);
         hex::encode(hasher.as_ref().to_vec())
+    }
+
+    fn calculate_merkel_root(transaction: &[Transaction]) -> String {
+        if transaction.is_empty() {
+            "0".to_string()
+        }
+
+        //TODO: Implement merkle root calculation
+
+        "0".to_string() // dummy value
     }
 }
