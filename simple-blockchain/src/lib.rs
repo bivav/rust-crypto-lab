30e505c33511ac81302e36bdae6fcc8bb31d5c61
bivav <bivavraj@gmail.com>
2024-02-20 17:50:18 +0000

    Created hashing function

--- simple-blockchain/src/lib.rs
+++ simple-blockchain/src/lib.rs
@@ -1,3 +1,6 @@
+use ring::digest;
+use ring::digest::SHA256;
+
 struct Block {
     index: u32,
     timestamp: u64,
@@ -14,15 +17,25 @@ pub struct Blockchain {
 impl Blockchain {
     pub fn new() -> Self {
         let mut chain = Vec::new();
-        let genesis_block = Block {
+        let genesis_block = Self::genesis_block();
+        chain.push(genesis_block);
+
+        Self { chain }
+    }
+
+    fn genesis_block() -> Block {
+        Block {
             index: 0,
             timestamp: 0,
             data: "Genesis Block".to_string(),
             previous_hash: "0".to_string(),
             hash: "0".to_string(),
             nonce: 0,
-        };
-        chain.push(genesis_block);
-        Self { chain }
+        }
+    }
+
+    fn calculate_hash(data: &[u8]) -> String {
+        let hasher = digest::digest(&SHA256, data);
+        hex::encode(hasher.as_ref().to_vec())
     }
 }
